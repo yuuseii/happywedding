370389d45b5bfd1e08d9ab70848e540b9c9461ec
yuuseii <62832017+yuuseii@users.noreply.github.com>
2020-03-31 14:56:02 +0800

    add btn bgimg
    smartphone part

--- js/wedding.js
+++ js/wedding.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 	$('.btn').click(function(event) {
 		/* Act on the event */
-		$(this).delay(1000).fadeOut();
+		$(this).delay(500).fadeOut();
+		$('.bgpic').fadeOut();
+		$('.touch').fadeOut();
 		$('.righthand, .lefthand').toggleClass('active');
 		$('.name').fadeOut('slow', function() {
 			
 		});
+
 		$('.card').fadeIn(3000);
 		$('.couple').fadeIn(3000);
 		$('.light').show().toggleClass('animated fadeInDown');
+		
 
 
 	});
